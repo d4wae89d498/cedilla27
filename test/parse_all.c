55cc5b54dc8a423db311f9d975bf5177cb334545
MarcFaussurier <faussurier.marc@icloud.com>
2022-12-17 22:29:55 +0100

    fix parse_all test

--- test/parse_all.c
+++ test/parse_all.c
@@ -34,9 +34,9 @@ int main()
     
     ast_node_list *list = parse_all("ab42ab");
 
-    assert(str_is_prefixed(list->data->src, "ab"));
-    assert(str_is_prefixed(list->next->data->src, "42"));
-    assert(str_is_prefixed(list->next->next->data->src, "ab")); 
+    assert(str_is(list->data->src, "ab"));
+    assert(str_is(list->next->data->src, "42"));
+    assert(str_is(list->next->next->data->src, "ab")); 
     assert(!(list->next->next->next)); 
 
     assert(!parse_all("ab420ab"));
